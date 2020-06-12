efaa253bb7820c396ee46ea93c66c0d07b31c5c1
josemrb <work@josemrb.com>
2020-06-12 12:20:45 -0400

    Add Semaphore CI config

--- config/config.exs
+++ config/config.exs
@@ -2,13 +2,13 @@ import Config
 
 if Mix.env() == :test do
   config :ex_message_db, ExMessageDB.Repo,
-    database: "message_store_test",
+    database: "ex_message_db_test",
     username: "postgres",
-    hostname: "localhost",
+    hostname: "0.0.0.0",
     pool: Ecto.Adapters.SQL.Sandbox
 else
   config :ex_message_db, ExMessageDB.Repo,
-    database: "message_store",
+    database: "ex_message_db",
     username: "postgres",
-    hostname: "localhost"
+    hostname: "0.0.0.0"
 end
